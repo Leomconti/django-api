0467c1e3ba2fa6753504870f4d7439019529c113
Leonardo Mosimann Conti <leomconti@gmail.com>
2023-09-15 14:14:52 -0300

    removing admin

--- api_seazone/api_core/admin.py
+++
@@ -1,8 +0,0 @@
-from anuncios.models import Anuncio
-from django.contrib import admin
-from imoveis.models import Imovel
-from reservas.models import Reserva
-
-admin.site.register(Imovel)
-admin.site.register(Anuncio)
-admin.site.register(Reserva)
