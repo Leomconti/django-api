3e148a5b3c77bda28b7e3fc0970891d29b4beab7
Leonardo Mosimann Conti <leomconti@gmail.com>
2023-09-14 23:00:28 -0300

    adjusting reservas codigo generation

--- api_seazone/reservas/models.py
+++ api_seazone/reservas/models.py
@@ -1,12 +1,20 @@
-import uuid
+import random
+from datetime import datetime
 
 from anuncios.models import Anuncio
 from django.db import models
 from rest_framework import serializers
 
 
+def generate_reservation_code():
+    """ as we are doing a code for a reservation, let's use the date it was created at, a random number just in case"""
+    # Current date in YYMMDD format
+    date_str = datetime.now().strftime("%y%m%d")
+
+    return int(date_str + str(random.randint(100000, 999999)))
+
 class Reserva(models.Model):
-    codigo = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)  # create a random unique id for each reservation
+    codigo = models.PositiveIntegerField(primary_key=True, default=generate_reservation_code(), unique=True, editable=False)  # create a random unique id for each reservation
     anuncio = models.ForeignKey(Anuncio, on_delete=models.DO_NOTHING)  # one to many relationship
     data_checkin = models.DateField()
     data_checkout = models.DateField()
